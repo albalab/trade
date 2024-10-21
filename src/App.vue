2fce6be75d573915d42404c732cced9d05df1a84
albalab <albalab@yandex.ru>
2024-10-22 01:09:04 +0400

    Added postMessage Listener

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Новый блок для отображения сообщений -->
+    <div v-if="receivedMessage">
+      <h3>Полученное сообщение:</h3>
+      <p>{{ receivedMessage }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +18,30 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      receivedMessage: null, // Храним полученное сообщение
+    };
+  },
+  methods: {
+    // Метод для обработки входящих сообщений от iframe
+    handleMessage(event) {
+      // Проверим источник сообщения, чтобы убедиться, что это наш iframe
+      // Можете добавить проверку event.origin для безопасности
+      if (event.data && typeof event.data === 'object') {
+        // Обновляем состояние, чтобы показать сообщение в интерфейсе
+        this.receivedMessage = `Время: ${event.data.time}, Данные: ${event.data.data}`;
+      }
+    }
+  },
+  mounted() {
+    // Добавляем слушатель на событие message
+    window.addEventListener('message', this.handleMessage);
+  },
+  beforeDestroy() {
+    // Удаляем слушатель при уничтожении компонента
+    window.removeEventListener('message', this.handleMessage);
   }
 }
 </script>
