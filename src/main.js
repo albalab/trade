3dbd5dc364f0dc8a65c1c492ba9f7a378c10303a
albalab <albalab@yandex.ru>
2024-11-14 11:21:32 +0300

    Added pinia

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './routes'; // Импортируем роутер
-import { createHead } from '@vueuse/head'; // Импортируем head из vueuse
+import router from './routes';
+import { createHead } from '@vueuse/head';
 
-// Создаем экземпляр head
+import { createPinia } from 'pinia';
+import piniaPersistedState from 'pinia-plugin-persistedstate';
+
 const head = createHead();
+const pinia = createPinia();
 
-// Создаем приложение Vue и подключаем роутер и head
+// Подключаем плагин к Pinia
+pinia.use(piniaPersistedState);
+
 const app = createApp(App);
+
 app.use(router);
 app.use(head);
+
+// Подключаем Pinia к приложению
+app.use(pinia);
+
 app.mount('#app');
