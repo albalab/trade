ee49d0377d408c31080a20288cd05506094ce1bf
albalab <albalab@yandex.ru>
2024-11-19 23:18:53 +0300

    Added select ticker by iframe

--- public/trade-scripts/selectTickerByIframe.js
+++ public/trade-scripts/selectTickerByIframe.js
@@ -1,6 +1,7 @@
 window.addEventListener("message", (event) => {
     // Проверяем, что сообщение содержит нужный тип
     if (event.data && event.data.selectTicker) {
+        changeGroupSymbol('tr[object HTMLTableRowElement]', 'group1', event.data.selectTicker);
         console.log("selectTicker", event.data.selectTicker);
     } else {
         //console.log("Другое сообщение:", event.data);
