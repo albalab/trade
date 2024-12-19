01178e5ad3ccdb74e4eabc75b5e71fb866381254
albalab <albalab@yandex.ru>
2024-12-19 06:33:37 +0400

    Refactoring

--- src/components/AlorOrderbooks.vue
+++ src/components/AlorOrderbooks.vue
@@ -52,16 +52,6 @@ export default {
 
     handleOrderbooksUpdate(data){
 
-      /*const newCandles = data.filter((item) => item.type === "newCandles");
-      const collectedClosePrice = data.find((item) => item.type === "collectedClosePrice");
-      const candlesMetrics = data.find((item) => item.type === "candlesMetrics");
-      const candlesStats = data.find((item) => item.type === "candlesStats");
-      const accumulatedCandlesStats = data.find((item) => item.type === "accumulatedCandlesStats");
-      const candleCounter = data.find((item) => item.type === "candleCounter");
-      const itemsFixedArray = data.find((item) => item.type === "itemsFixedArray");*/
-
-      //const sortedOrderbooksStats = data.filter(item => item.type === 'sortedOrderbooksStats');
-
       const newOrderbooks = data.filter((item) => item.type === "newOrderbooks");
       const orderbooksMetrics = data.filter(item => item.type === 'orderbooksMetrics');
       const orderbooksStats = data.find((item) => item.type === "orderbooksStats");
@@ -77,52 +67,13 @@ export default {
       this.orderbooksStore.orderbooksFixedArray = itemsFixedArray.data;
       this.orderbooksStore.orderbooksCounter = orderbooksCounter.data;
 
-      /*
-      this.orderbooksStore.orderbooksMetrics = orderbooksMetrics.length ? orderbooksMetrics[0].data : {}
-      this.sortedOrderbooksStats = sortedOrderbooksStats.length ? sortedOrderbooksStats[0].data : {}
-      this.orderbooksStore.newOrderbooks = newOrderbooks[0].data;
-      this.orderbooksStore.orderbooksMetrics = orderbooksMetrics.data;
-      this.orderbooksStore.accumulatedOrderbooksStats = accumulatedOrderbooksStats.data;
-      this.orderbooksStore.orderbooksStats = orderbooksStats.data;
-      this.orderbooksStore.orderbooksFixedArray = itemsFixedArray.data;
-      this.orderbooksStore.orderbooksCounter = orderbooksCounter.data;*/
-
     },
 
-    /*connectToWebSocket() {
-      // Устанавливаем соединение с WebSocket сервером
-      const socket = new WebSocket('wss://signalfabric.com/datastream/');
-
-      // Обрабатываем получение сообщений от WebSocket сервера
-      socket.onmessage = (event) => {
-
-        let data = JSON.parse(event.data);
-        data = data?.aggregatedOrderbooks;
-
-        if (!Array.isArray(data)) this.handleOrderbooksData(data);
-
-      };
-
-      socket.onopen = () => {
-        console.log('Connected to WebSocket');
-      };
-
-      socket.onerror = (error) => {
-        console.error('WebSocket error:', error);
-      };
-
-      socket.onclose = () => {
-        console.log('WebSocket connection closed');
-      };
-    }*/
   },
 
   mounted() {
-
     webSocketService.connect();
     webSocketService.subscribe("aggregatedOrderbooks", this.handleOrderbooksUpdate);
-
-    //this.connectToWebSocket();
   },
 
 };
